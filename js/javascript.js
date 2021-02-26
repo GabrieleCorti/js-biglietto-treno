0e836b55d3ffff9ec1da7209c20d031ffd40b66e
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-02-26 16:04:18 +0100

    calcolo base completato

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,8 @@
-alert("lmao")
+//quanti km vuole fare
+var km = parseInt(prompt("quanti Km devi percorrere?"));
+//console.log(km);
+//quanti anni ha
+var anni = parseInt(prompt("quanti anni hai?"));
+//calcolo prezzo
+var prezzo = (km * 0.21);
+console.log(prezzo);
