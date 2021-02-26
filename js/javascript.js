8afdbb2272149f88b79e86b8f31c62c5ab9c88b0
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-02-26 16:32:03 +0100

    calcolo con le varie condizoni completato

--- js/javascript.js
+++ js/javascript.js
@@ -5,4 +5,19 @@ var km = parseInt(prompt("quanti Km devi percorrere?"));
 var anni = parseInt(prompt("quanti anni hai?"));
 //calcolo prezzo
 var prezzo = (km * 0.21);
-console.log(prezzo);
+//sconto del 40% per over 65 e del 20% per under 18
+if ( anni > 65 ) {
+
+  prezzo = (prezzo * 0.6);
+  alert("il tuo biglietto ha un prezzo di " + prezzo.toFixed(2) +"£");
+
+} else if ( anni < 18) {
+
+  prezzo = (prezzo * 0.8)
+  alert("il tuo biglietto ha un prezzo di " + prezzo.toFixed(2) +"£");
+
+} else {
+
+  alert("il tuo biglietto ha un prezzo di " + prezzo.toFixed(2) +"£");
+
+};
